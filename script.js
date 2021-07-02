754ed03bf90646f0cf2ade59ab14db15f94bdd5b
par94 <piskunov57@gmail.com>
2021-07-02 09:23:32 +0300

    Fixed all the issues, works as intended now

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const container = document.querySelector('.container');
 console.log(container);
 
 function Highlight (e) {
-    //console.log(e);
-    //console.log(this);
     this.classList.add("highlighted");
 };
 
@@ -20,33 +18,24 @@ function Reset (e) {
         } while ((size == NaN) || (size > 100) || (size < 2));
     }
 
-    console.log(container.childNodes);
-
     while (container.firstChild) {
         container.removeChild(container.lastChild);
       }
 
     container.style['grid-template-columns'] = `repeat(${size}, 1fr)`;
 
-    console.log(container.childNodes);
-
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             const div = document.createElement('div');
             div.classList.add('griditem');
-            div.textContent = `${i}, ${j}`;
+            //div.textContent = `${i}, ${j}`;
             container.appendChild(div);
         }
     }
 
-    console.log(container.childNodes);
-
-    console.log(e);
-    console.log(size);
+    const gridItems = Array.from(document.querySelectorAll('.griditem'));
+    gridItems.forEach(key => key.addEventListener("mouseleave", Highlight));
 };
 
 Reset();
-const gridItems = Array.from(document.querySelectorAll('.griditem'));
-gridItems.forEach(key => key.addEventListener("mouseleave", Highlight));
 document.getElementById("resetButton").addEventListener("click", Reset);
-console.log(container.childNodes);
